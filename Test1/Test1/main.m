91632ed0732052717fe1c2abdf1a9aa2b954326d
周亮 <zhouliang@ehhaoyao.com>
2018-12-16 21:58:17 +0800

    no message

---
+++ Test1/Test1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test1
+//
+//  Created by 周亮 on 2018/12/16.
+//  Copyright © 2018 周亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
